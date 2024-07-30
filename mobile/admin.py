68475ee876ca135b80104f4abd1a7ef50371d45f
hosein <hoseinlaki16@gmail.com>
2024-07-30 15:36:43 +0330

    Create models and add it to admin panel

--- mobile/admin.py
+++ mobile/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand, Phone
+
+@admin.register(Brand)
+class BrandModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'nationality')
+
+
+@admin.register(Phone)
+class PhoneModelAdmin(admin.ModelAdmin):
+    list_display = (
+        'brand',
+        'model',
+        'price',
+        'color',
+        'screen_size',
+        'status',
+        'country_of_origin'
+    )
--- mobile/models.py
+++ mobile/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Phone(models.Model):
+    MOBILE_STATUS_AVAILABLE = 'available'
+    MOBILE_STATUS_UNAVAILABLE = 'unavailable'
+    
+    MOBILE_STATUS = [
+        (MOBILE_STATUS_AVAILABLE, 'موجود'),
+        (MOBILE_STATUS_UNAVAILABLE, 'ناموجود')
+    ]
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='phones')
+    model = models.CharField(max_length=255, unique=True)
+    price = models.PositiveIntegerField()
+    color = models.CharField(max_length=50)
+    screen_size = models.PositiveIntegerField()
+    status = models.CharField(max_length=11,
+                              choices=MOBILE_STATUS,
+                              default=MOBILE_STATUS_AVAILABLE)
+    country_of_origin = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return f'{self.brand}: {self.model}'
