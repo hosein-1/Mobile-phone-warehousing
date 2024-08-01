d5e9eea667da89531e7ac6afc671daccc5117278
hosein <hoseinlaki16@gmail.com>
2024-08-01 18:26:21 +0330

    Change url of mobile list view

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from mobile import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mobile/', include('mobile.urls')),
+    path('', views.MobileListView.as_view(), name='mobiles_list'),
 ]
--- mobile/urls.py
+++ mobile/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'mobile'
 
 urlpatterns = [
-    path('list/', views.MobileListView.as_view(), name='mobiles_list'),
     path('create/', views.MobileCreateView.as_view(), name='mobile_create'),
     path('brands/list', views.BrandListView.as_view(), name='brands_list'),
     path('brand/create/', views.BrandCreateView.as_view(), name='brand_create'),
