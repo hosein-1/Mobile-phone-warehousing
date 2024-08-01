723733f4ab239247185ddc5eb8d84c32bc06c4e9
hosein <hoseinlaki16@gmail.com>
2024-08-01 12:03:56 +0330

    Addbrand list view and create view with templates

--- mobile/forms.py
+++ mobile/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
-from .models import Mobile
+from .models import Mobile, Brand
 
-class MobileForm(forms.ModelForm):
+class MobileCreateForm(forms.ModelForm):
     class Meta:
         model = Mobile
         fields = ['brand', 'model', 'price', 'color', 'screen_size', 'status', 'country_of_origin']
-        
+
+
+class BrandCreateForm(forms.ModelForm):
+    class Meta:
+        model = Brand
+        fields = ['name', 'nationality']
--- mobile/views.py
+++ mobile/views.py
@@ -3,21 +3,36 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
 
 from .models import Mobile, Brand
-from .forms import MobileForm
+from .forms import MobileCreateForm, BrandCreateForm
 
 class MobileListView(ListView):
     model = Mobile
     template_name = 'mobile/mobiles_list.html'
     
-    
+
 class MobileCreateView(CreateView):
     model = Mobile
     template_name = 'mobile/create_mobile.html'
-    form_class = MobileForm
+    form_class = MobileCreateForm
     success_url = reverse_lazy('mobile:mobiles_list')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['brands'] = Brand.objects.all()
         return context
+
+
+class BrandListView(ListView):
+    model = Brand
+    template_name = 'mobile/brands_list.html'
+    context_object_name = 'brands_queryset'
+
+
+class BrandCreateView(CreateView):
+    model = Brand
+    template_name = 'mobile/create_brand.html'
+    form_class = BrandCreateForm
+    success_url = reverse_lazy('mobile:brands_list')
+
+
     
