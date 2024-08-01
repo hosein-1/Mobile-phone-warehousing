a23b0536d94f9e2b6d8d06f9ac08db653419c558
hosein <hoseinlaki16@gmail.com>
2024-08-01 18:40:03 +0330

    Add messages

--- mobile/views.py
+++ mobile/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .models import Mobile, Brand
 from .forms import MobileCreateForm, BrandCreateForm
+
 
 class MobileListView(ListView):
     model = Mobile
     template_name = 'mobile/mobiles_list.html'
     context_object_name = 'mobiles_queryset'
     
+    
 
-class MobileCreateView(CreateView):
+class MobileCreateView(SuccessMessageMixin, CreateView):
     model = Mobile
     template_name = 'mobile/create_mobile.html'
     form_class = MobileCreateForm
-    success_url = reverse_lazy('mobile:mobiles_list')
+    success_url = reverse_lazy('mobiles_list')
+
+    def get_success_message(self, cleaned_data):
+        return f'برند <<{self.object.model}>> با موفقیت ذخیره شد.'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -29,11 +35,14 @@ class BrandListView(ListView):
     context_object_name = 'brands_queryset'
 
 
-class BrandCreateView(CreateView):
+class BrandCreateView(SuccessMessageMixin, CreateView):
     model = Brand
     template_name = 'mobile/create_brand.html'
     form_class = BrandCreateForm
     success_url = reverse_lazy('mobile:brands_list')
+    
+    def get_success_message(self, cleaned_data):
+        return f'برند <<{self.object.name}>> با موفقیت ذخیره شد.'
 
 
     
